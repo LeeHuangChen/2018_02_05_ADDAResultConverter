568148ddb61e5ce24ddd80a249ae2e543485080f
Lee <lhc1@rice.edu>
2018-02-05 13:45:09 -0600

    Implementation.

--- Configurations.py
+++ Configurations.py
@@ -1,3 +1,8 @@
+
+addaResultDir = "InputFiles/nrbd_PFam_Out_ADDA_Result.txt"
+nidToPidDir = "InputFiles/nrbd_PFam_Out_ProtToNid.txt"
+
+
 inputFolder = "InputFiles"
 generateFolder = "Generated"
 resultFolder = "Results"
--- run.py
+++ run.py
@@ -1,8 +1,42 @@
 import Configurations as conf
+from src import util
+from cPickle import dump
+import os
+
+
+def genNidToPidDict():
+    nidToPidDict = {}
+    with open(conf.nidToPidDir, "r") as f:
+        for line in f:
+            if len(line)>0:
+                arr = line.split("\t")
+                nid = int(arr[0].strip())
+                pid = arr[1].strip()
+                nidToPidDict[nid] = pid
+    return nidToPidDict
 
 
 def main():
-    pass
+    resultDict = {}
+    nidToPidDict = genNidToPidDict()
+
+    with open(conf.addaResultDir, "r") as f:
+        for line in f:
+            arr = line.split("\t")
+            nid = int(arr[0].strip())
+            s = int(arr[1].strip())
+            e = int(arr[2].strip())
+            pid = nidToPidDict[nid]
+
+            if pid in resultDict.keys():
+                # adda did not provide domain family info, so I just put '1' here
+                resultDict[pid].append([1, s, e])
+            else:
+                resultDict[pid] = [[1, s, e]]
+
+    util.generateDirectories(conf.resultFolder)
+    with open(os.path.join(conf.resultFolder, "result.cpickle"), "wb") as f:
+        dump(resultDict, f)
 
 
 if __name__ == '__main__':
